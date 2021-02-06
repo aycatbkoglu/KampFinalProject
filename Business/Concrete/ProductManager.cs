fbfa3c3627e7dd7a94f08bdd40c4ff4d675b8e0d
Ayça <50514082+aycatbkoglu@users.noreply.github.com>
2021-02-06 14:12:21 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,39 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş kodları
+            //Yetkisi var mı?
+
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            throw new NotImplementedException();
+        }
+    
+    }
+}
